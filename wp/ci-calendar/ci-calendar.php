2f90ee2db0943d8956b504cf48269fe6d5843d9f
Phillip Oertel <me@phillipoertel.com>
2026-04-03 13:07:05 +0200

    Fix cache buster

--- wp/ci-calendar/ci-calendar.php
+++ wp/ci-calendar/ci-calendar.php
@@ -15,16 +15,15 @@ function ci_calendar_shortcode() {
     $response = wp_remote_get($url);
     $ical = wp_remote_retrieve_body($response);
 
-    wp_enqueue_script('my-calendar', plugin_dir_url(__FILE__) . 'calendar.js', [], filemtime(plugin_dir_path(__FILE__) . 'calendar.js'), true);
-    wp_enqueue_style('my-calendar', plugin_dir_url(__FILE__) . 'calendar.css', [], filemtime(plugin_dir_path(__FILE__) . 'calendar.css'));
-    
-    wp_localize_script('my-calendar', 'CalendarData', ['ical' => $ical]);
-    
+    $ver_css = filemtime(plugin_dir_path(__FILE__) . 'ci-calendar.css');
+    $ver_js  = filemtime(plugin_dir_path(__FILE__) . 'ci-calendar.js');
+    $ver_cc  = filemtime(plugin_dir_path(__FILE__) . 'country-codes.js');
+
     ob_start(); ?>
-    <link rel="stylesheet" href="<?php echo esc_url(plugin_dir_url(__FILE__) . 'ci-calendar.css'); ?>">
+    <link rel="stylesheet" href="<?php echo esc_url(plugin_dir_url(__FILE__) . 'ci-calendar.css'); ?>?v=<?php echo $ver_css; ?>">
     <?php echo file_get_contents(plugin_dir_path(__FILE__) . 'ci-calendar.html'); ?>
-    <script src="<?php echo esc_url(plugin_dir_url(__FILE__) . 'country-codes.js'); ?>"></script>
-    <script src="<?php echo esc_url(plugin_dir_url(__FILE__) . 'ci-calendar.js'); ?>"></script>
+    <script src="<?php echo esc_url(plugin_dir_url(__FILE__) . 'country-codes.js'); ?>?v=<?php echo $ver_cc; ?>"></script>
+    <script src="<?php echo esc_url(plugin_dir_url(__FILE__) . 'ci-calendar.js'); ?>?v=<?php echo $ver_js; ?>"></script>
     <script>
         initCalendar(<?php echo json_encode($ical); ?>);
     </script>
